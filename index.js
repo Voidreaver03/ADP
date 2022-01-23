5768eb8fc9afa3a073015019ad234ababff05b29
Voidreaver03 <paologan2003@gmail.com>
2022-01-23 22:01:37 +0100

    ok3

--- index.js
+++ index.js
@@ -43,7 +43,60 @@ app.post("/deleteAccount", (req, res) => {
     }
     });
     });
-app.listen(port, host, () => {
+
+    app.post("/addADP", (req, res) => {
+        var user = req.body.user;
+        var pw = req.body.pw;
+        var ADP = req.body.ADP;
+        
+        fs.readFile("user.json", "utf8", (err, jsonString) => {
+        if(err) {
+        console.log("Error");
+        }
+
+
+        var a = JSON.parse(jsonString)
+        if(a["user"]==user && a["pw"]==pw) {
+        a["ADP"]=ADP;
+        fs.writeFileSync("user.json", "    ");
+        var b = JSON.stringify(b);
+        fs.writeFileSync("user.json", b);
+        res.send("ADP works");
+        }else{
+        res.send("Error");
+        }
+        });
+        });
+
+        app.post("/addMeet", (req, res) => {
+            var user = req.body.user;
+            var pw = req.body.pw;
+            var name = req.body.name;
+            var date = req.body.date;
+            var hour = req.body.hour;
+            var ADP = req.body.ADP;
+
+
+            fs.readFile("user.json", "utf8", (err, jsonString) => {
+            if(err) {
+            console.log("Error");
+            }
+
+            
+            var a = JSON.parse(jsonString);
+            if(a["user"]==user && a["pw"]==pw) {
+            var a = "name: "+name+ " , "+ "ADPmeet: "+ADP+ " , " +" date: "+date+ " , " +"hour: "+hour;
+            var b = JSON.stringify(b);
+            fs.writeFileSync("meet.json", a);
+            res.send("Meet create");
+            }else{
+            res.send("Error");
+            }
+            });
+            });
+            
+
+    app.listen(port, host, () => {
     console.log("server running at http://%s:%d", host, port);
     });
 
